875b15887a90565e04475d41c291047da2561fcf
Sgiane11a <steyci.cordova@tecsup.edu.pe>
2025-04-17 00:33:21 -0500

    Configuración del proyecto Actualizado

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app01',  #Primera Aplicación del Proyecto
+    'rest_framework',  # El módulo REST Framework
 ]
 
 MIDDLEWARE = [
