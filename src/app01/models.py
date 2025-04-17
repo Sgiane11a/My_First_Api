29d2e312d0856c50a006a715368f8a77c295f239
Sgiane11a <steyci.cordova@tecsup.edu.pe>
2025-04-17 00:52:22 -0500

    models.py modificado

--- src/app01/models.py
+++ src/app01/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class App (models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    tecnology = models.CharField(max_length=200)
+    craeted_at = models.DateTimeField(auto_now_add=True)
